cdbf21d5a77d5044fff6dd66b5cc605d92dd4204
Daniel Bautista <erick.olano@gmail.com>
2026-03-11 22:00:51 -0500

    fin de la seccion 7 - optimizacion y despliegue

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,16 +1,25 @@
 // Este es mi archivo de useGifs
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.actions";
 
-const useGifs = () => {
+// const gifsCache: Record<string, Gif[]> = {};
 
-    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+const useGifs = () => {
 
     const [gifs, setGifs] = useState<Gif[]>([]);
 
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
+    const gifsCache = useRef<Record<string, Gif[]>>({});
+
     const handleTermClicked = async (term: string) => {
         // console.log({term});
+        if(gifsCache.current[term]) {
+            setGifs(gifsCache.current[term]);
+            return;
+        }
+
         const gifs = await getGifsByQuery(term);
         setGifs(gifs);
     }
@@ -50,7 +59,8 @@ const useGifs = () => {
         const gifs = await getGifsByQuery(query);
         setGifs(gifs);    
 
-
+        gifsCache.current[query] = gifs;
+        // console.log({gifsCache});
 
     }
 
